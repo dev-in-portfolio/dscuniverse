979eb91d3af4ce9364d24c96c60c6c84f25c74f8
dev-in-portfolio <devin.dev.portfolio@gmail.com>
2026-09-08 10:49:38 -0400

    Build Universe and recognition pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+        recognition: resolve(process.cwd(), 'recognition.html'),
+      },
+    },
   },
 });
